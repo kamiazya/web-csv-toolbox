2a05a79d0cccd31944defa62599d17ce49e1d41e
kamiazya <yuki@kamiazya.tech>
2023-11-30 14:02:14 +0900

    chaos: add test case

--- src/LexerTransformer.test.ts
+++ src/LexerTransformer.test.ts
@@ -24,21 +24,108 @@ describe("LexerTransformer", () => {
   }
 
   describe("Property Based Testing", () => {
-    test("Field", async () => {
-      await fc.assert(
-        fc.asyncProperty(
-          fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
-          async (s) => {
-            const result = await transform(new LexerTransformer(), s);
-            expect(result).toStrictEqual([
-              {
-                type: Field,
-                value: s,
-              },
-            ]);
-          }
-        )
-      );
+    describe("Field", () => {
+      test("string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), s);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), `"${s}"`);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), `"${s},${s}"`);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), s);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), `"${s}"`);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(new LexerTransformer(), `"${s},${s}"`);
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
     });
   });
 });
