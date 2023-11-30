5e1cca37f7ad0fe3097a35da3a31b20324bb2675
kamiazya <yuki@kamiazya.tech>
2023-11-30 14:13:19 +0900

    chaos: add test cases

--- src/LexerTransformer.test.ts
+++ src/LexerTransformer.test.ts
@@ -1,10 +1,13 @@
 import { describe, expect } from "vitest";
 import { test, fc } from "@fast-check/vitest";
-import { LexerTransformer } from './LexerTransformer';
-import { Field } from "./common/constants";
+import { LexerTransformer } from "./LexerTransformer";
+import { Field, FieldDelimiter, RecordDelimiter } from "./common/constants";
 
 describe("LexerTransformer", () => {
-  async function transform(transformer: LexerTransformer, s: string): Promise<any[]> {
+  async function transform(
+    transformer: LexerTransformer,
+    s: string
+  ): Promise<any[]> {
     const result: any[] = [];
     await new ReadableStream({
       start(controller) {
@@ -64,7 +67,10 @@ describe("LexerTransformer", () => {
           fc.asyncProperty(
             fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
             async (s) => {
-              const result = await transform(new LexerTransformer(), `"${s},${s}"`);
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}"`
+              );
               expect(result).toStrictEqual([
                 {
                   type: Field,
@@ -79,7 +85,9 @@ describe("LexerTransformer", () => {
       test("unicode string", async () => {
         await fc.assert(
           fc.asyncProperty(
-            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
             async (s) => {
               const result = await transform(new LexerTransformer(), s);
               expect(result).toStrictEqual([
@@ -96,7 +104,9 @@ describe("LexerTransformer", () => {
       test("unicode string with double quote", async () => {
         await fc.assert(
           fc.asyncProperty(
-            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
             async (s) => {
               const result = await transform(new LexerTransformer(), `"${s}"`);
               expect(result).toStrictEqual([
@@ -113,13 +123,375 @@ describe("LexerTransformer", () => {
       test("unicode string with comma", async () => {
         await fc.assert(
           fc.asyncProperty(
-            fc.unicodeString().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
             async (s) => {
-              const result = await transform(new LexerTransformer(), `"${s},${s}"`);
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}"`
+              );
               expect(result).toStrictEqual([
                 {
                   type: Field,
                   value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+    });
+
+    describe("FieldDelimiter", () => {
+      test("string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `${s},${s}`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `${s},${s}`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s}","${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s}","${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}","${s},${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}","${s},${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with double quote and comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}","${s},${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with double quote and comma", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s},${s}","${s},${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+                {
+                  type: FieldDelimiter,
+                  value: ",",
+                },
+                {
+                  type: Field,
+                  value: `${s},${s}`,
+                },
+              ]);
+            }
+          )
+        );
+      });
+    });
+
+    describe.each([
+      { title: "EOL=LF, EOF=true", EOL: "\n", EOF: true },
+      { title: "EOL=LF, EOF=false", EOL: "\n", EOF: false },
+      { title: "EOL=CRLF, EOF=true", EOL: "\r\n", EOF: true },
+      { title: "EOL=CRLF, EOF=false", EOL: "\r\n", EOF: false },
+    ])("RecordDelimiter($title)", ({ EOL, EOF }) => {
+      test("string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `${s}${EOL}${s}`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: RecordDelimiter,
+                  value: EOL,
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `${s}${EOL}${s}`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: RecordDelimiter,
+                  value: EOL,
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc.string().filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s}"${EOL}"${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: RecordDelimiter,
+                  value: EOL,
+                },
+                {
+                  type: Field,
+                  value: s,
+                },
+              ]);
+            }
+          )
+        );
+      });
+
+      test("unicode string with double quote", async () => {
+        await fc.assert(
+          fc.asyncProperty(
+            fc
+              .unicodeString()
+              .filter((s) => /[\",]/gi.test(s) === false && s !== ""),
+            async (s) => {
+              const result = await transform(
+                new LexerTransformer(),
+                `"${s}"${EOL}"${s}"`
+              );
+              expect(result).toStrictEqual([
+                {
+                  type: Field,
+                  value: s,
+                },
+                {
+                  type: RecordDelimiter,
+                  value: EOL,
+                },
+                {
+                  type: Field,
+                  value: s,
                 },
               ]);
             }
