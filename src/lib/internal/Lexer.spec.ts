ddda068058caf2a04322543a8a8868f35a4692b3
kamiazya <yuki@kamiazya.tech>
2024-01-15 01:56:27 +0900

    Add failed test case for Lexer class with specific seed and path

--- src/lib/internal/Lexer.spec.ts
+++ src/lib/internal/Lexer.spec.ts
@@ -147,6 +147,11 @@ describe("class Lexer", () => {
           expect(actual).toStrictEqual(expected);
         },
       ),
+      {
+        seed: 77906161,
+        path: "62:5:4:6:5:5:7:9:5:5:12:11:11:11",
+        endOnFailure: true,
+      },
     );
   });
 
