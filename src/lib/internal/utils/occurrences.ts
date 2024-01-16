9589a479d4edd72b19b74462bbbd8cd231ba285f
kamiazya <yuki@kamiazya.tech>
2024-01-16 23:20:56 +0900

    Fix linting issues and optimize code

--- src/lib/internal/utils/occurrences.ts
+++ src/lib/internal/utils/occurrences.ts
@@ -9,13 +9,12 @@ const PATTERN_CACHE = new Map<string, RegExp>();
  * @returns The number of occurrences.
  */
 export function occurrences(string: string, substring: string) {
-  let pattern: RegExp;
-  if (PATTERN_CACHE.has(substring)) {
-    // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    pattern = PATTERN_CACHE.get(substring)!;
-  } else {
-    pattern = new RegExp(`(?=(${escapeRegExp(substring)}))`, "g");
-    PATTERN_CACHE.set(substring, pattern);
+  if (!PATTERN_CACHE.has(substring)) {
+    PATTERN_CACHE.set(
+      substring,
+      new RegExp(`(?=(${escapeRegExp(substring)}))`, "g"),
+    );
   }
+  const pattern = PATTERN_CACHE.get(substring)!;
   return Array.from(string.matchAll(pattern), ([, v]) => v).length;
 }
