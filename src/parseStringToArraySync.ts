1f22cfe2fec4369d384307413183615c7f9850f0
nagasawaryoya <nagasawaryoya@gmail.com>
2024-04-16 01:01:59 +0900

    refactor: #69 Dynamic Type Inference and User-Defined Types from CSV Headers - parseStringToArraySync

--- src/parseStringToArraySync.ts
+++ src/parseStringToArraySync.ts
@@ -1,7 +1,47 @@
 import { Lexer } from "./Lexer.ts";
 import { RecordAssembler } from "./RecordAssembler.ts";
-import type { CSVRecord, ParseOptions } from "./common/types.ts";
+import type {
+  CSVRecord,
+  ParseOptions,
+  PickCSVHeader,
+  ToParsedCSVRecords,
+} from "./common/types.ts";
+import type { COMMA, DOUBLE_QUOTE } from "./constants.ts";
 
+export function parseStringToArraySync<
+  CSVSource extends string,
+  Delimiter extends string = typeof COMMA,
+  Quotation extends string = typeof DOUBLE_QUOTE,
+  Header extends ReadonlyArray<string> = PickCSVHeader<
+    CSVSource,
+    Delimiter,
+    Quotation
+  >,
+>(
+  csv: CSVSource,
+  options: ParseOptions<Header> & {
+    delimiter?: Delimiter;
+    quotation?: Quotation;
+  },
+): ToParsedCSVRecords<CSVSource, Delimiter, Quotation> extends infer R extends
+  CSVRecord<readonly string[]>[]
+  ? R
+  : CSVRecord<Header>[];
+export function parseStringToArraySync<
+  CSVSource extends string,
+  Header extends ReadonlyArray<string> = PickCSVHeader<CSVSource>,
+>(
+  csv: CSVSource,
+  options?: ParseOptions<Header>,
+): ToParsedCSVRecords<CSVSource> extends infer R extends CSVRecord<
+  readonly string[]
+>[]
+  ? R
+  : CSVRecord<Header>[];
+export function parseStringToArraySync<Header extends ReadonlyArray<string>>(
+  csv: string,
+  options?: ParseOptions<Header>,
+): CSVRecord<Header>[];
 export function parseStringToArraySync<Header extends ReadonlyArray<string>>(
   csv: string,
   options?: ParseOptions<Header>,
