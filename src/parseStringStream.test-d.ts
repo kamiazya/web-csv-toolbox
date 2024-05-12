c711e6b407346f661daa972eb0e483aa15281d28
nagasawaryoya <nagasawaryoya@gmail.com>
2024-05-12 22:43:58 +0900

    update test

--- src/parseStringStream.test-d.ts
+++ src/parseStringStream.test-d.ts
@@ -1,5 +1,4 @@
 import { describe, expectTypeOf, it } from "vitest";
-import { CR, CRLF, LF } from "./constants.ts";
 import {
   type CSVRecord,
   type ParseOptions,
@@ -35,17 +34,9 @@ describe("csv literal ReadableStream parsing", () => {
 Alice,24,New York,10001
 Bob,36,Los Angeles,90001`;
 
-  const csv2 = "name,age,city,zip";
-
   it("should csv header of the parsed result will be header's tuple", () => {
     expectTypeOf(
       parseStringStream(new ReadableStream<typeof csv1>()),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<CSVRecord<readonly ["name", "age", "city", "zip"]>>
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv2>()),
     ).toEqualTypeOf<
       AsyncIterableIterator<CSVRecord<readonly ["name", "age", "city", "zip"]>>
     >();
@@ -53,141 +44,24 @@ Bob,36,Los Angeles,90001`;
 });
 
 describe("csv literal ReadableStream parsing with line breaks, quotation, newline", () => {
-  const csv1 = `"name","age
-","city","zi
-p"
-Alice,24,New York,10001
-Bob,36,Los Angeles,90001`;
-
-  const csv2 = `'name'@'age
-'@'city'@'zi
-p'
-Alice@24@New York@10001
-Bob@36@Los Angeles@90001`;
-
-  const csv3 = `'name'@'age
-
-'@'c
-ity'@'
-zi
-p'
-Alice@'24'@'Ne
-w York'@'10
-001'
-Bob@36@'Los Ange
-
-les'@'9
-0001'`;
-
-  const csv4 = `'name'@'age
-
-'@'c
-ity'@'
-zi
-p'`;
-
-  const csv5 = `'na${CR}me'@'age'@'ci${CR}${CRLF}ty'@'z${LF}ip'${CR}Alice@24@'New${CR}${LF} York'@10001${LF}Bob@'3${CRLF}6'@'Los Angeles'@'90${CRLF}001'`;
-
-  const csv6 = `'@name'@'age
-
-'@'c
-@ity@'@'
-zi
-p'
-'Alice@'@'24'@'@Ne
-w York'@'10
-00@1'
-Bob@36@'Lo@s Ange
-
-les'@'@9
-0001'`;
-
-  const csv7 = `'@name'@'a'g'e
-
-'@'c
-@i''ty@'@'
-'zi
-p''
-'Al'ic'e@'@''24''@'@Ne
-w Yo'r'k'@'10
-00@1'
-'Bob'@36@'Lo@s A'nge'
-
-les'@'@9
-0001'''`;
+  const csv1 = `$name$*$*ag
+e
+$*$city$*$z*i
+p*$
+Alice*24*New York*$1000
+$1$
+Bob*$36$*$Los$
+Angeles$*90001`;
 
   it("should csv header of the parsed result will be header's tuple", () => {
     expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv1>()),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["name", "age\n", "city", "zi\np"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv2>(), {
-        delimiter: "@",
-        quotation: "'",
+      parseStringStream(new ReadableStream<typeof csv1>(), {
+        delimiter: "*",
+        quotation: "$",
       }),
     ).toEqualTypeOf<
       AsyncIterableIterator<
-        CSVRecord<readonly ["name", "age\n", "city", "zi\np"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv3>(), {
-        delimiter: "@",
-        quotation: "'",
-      }),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["name", "age\n\n", "c\nity", "\nzi\np"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv4>(), {
-        delimiter: "@",
-        quotation: "'",
-      }),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["name", "age\n\n", "c\nity", "\nzi\np"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv5>(), {
-        delimiter: "@",
-        quotation: "'",
-      }),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["na\rme", "age", "ci\r\r\nty", "z\nip"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv6>(), {
-        delimiter: "@",
-        quotation: "'",
-      }),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["@name", "age\n\n", "c\n@ity@", "\nzi\np"]>
-      >
-    >();
-
-    expectTypeOf(
-      parseStringStream(new ReadableStream<typeof csv7>(), {
-        delimiter: "@",
-        quotation: "'",
-      }),
-    ).toEqualTypeOf<
-      AsyncIterableIterator<
-        CSVRecord<readonly ["@name", "a'g'e\n\n", "c\n@i''ty@", "\n'zi\np'"]>
+        CSVRecord<readonly ["name", "*ag\ne\n", "city", "z*i\np*"]>
       >
     >();
   });
