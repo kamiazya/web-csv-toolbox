41f94ea7fea0533c302026e855c0f44cfe90a43f
kamiazya <yuki@kamiazya.tech>
2024-06-09 20:42:16 +0900

    refactor: Add ParseError class

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,9 +1,31 @@
+import type { Position } from "./types.js";
+
 /**
  * Error class for invalid option errors.
  */
 export class InvalidOptionError extends Error {
-  constructor(...args: ConstructorParameters<typeof Error>) {
-    super(...args);
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
     this.name = "InvalidOptionError";
+  }
+}
+
+export interface ParseErrorOptions extends ErrorOptions {
+  position?: Position;
+}
+
+/**
+ * Error class for parse errors.
+ */
+export class ParseError extends Error {
+  /**
+   * The position where the error occurred.
+   */
+  public position?: Position;
+
+  constructor(message?: string, options?: ParseErrorOptions) {
+    super(message, { cause: options?.cause });
+    this.name = "ParseError";
+    this.position = options?.position;
   }
 }
