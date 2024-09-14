c4602fdc9ef3c04b2e7c1addcd70b10eb2756098
kamiazya <yuki@kamiazya.tech>
2024-09-15 01:13:56 +0900

    Update benchmark tests names

--- packages/parser/src/lib/parseBinary.bench.ts
+++ packages/parser/src/lib/parseBinary.bench.ts
@@ -4,30 +4,33 @@ import { getBinaryCSV } from "#/tests/utils/data";
 
 import { parseBinary } from "./parseBinary.js";
 
-describe.each([50, 100, 500, 1_000])("%d rows", (rows) => {
-  const csv = getBinaryCSV(rows);
+describe.each([50, 100, 500, 1_000])("%d row", (row) => {
+  const csv = getBinaryCSV(row);
 
-  bench("parse a binary CSV into an AsyncIterableIterator", async () => {
-    for await (const _ of parseBinary(csv)) {
-      // noop
-    }
-  });
+  bench(
+    `parse a binary ${row} row CSV into an AsyncIterableIterator`,
+    async () => {
+      for await (const _ of parseBinary(csv)) {
+        // noop
+      }
+    },
+  );
 
-  bench("parse a binary CSV into an Array", () => {
+  bench(`parse a binary ${row} row CSV into an Array`, () => {
     parseBinary.toArraySync(csv);
   });
 
-  bench("parse a binary CSV into a Promise<Array>", async () => {
+  bench(`parse a binary ${row} row CSV into a Promise<Array>`, async () => {
     await parseBinary.toArray(csv);
   });
 
-  bench("parse a binary CSV into a IterableIterator", () => {
+  bench(`parse a binary ${row} row CSV into a IterableIterator`, () => {
     for (const _ of parseBinary.toIterableIterator(csv)) {
       // noop
     }
   });
 
-  bench("parse a binary CSV into a ReadableStream", async () => {
+  bench(`parse a binary ${row} row CSV into a ReadableStream`, async () => {
     await parseBinary.toStream(csv).pipeTo(
       new WritableStream({
         write(_) {
--- packages/parser/src/lib/parseString.bench.ts
+++ packages/parser/src/lib/parseString.bench.ts
@@ -4,30 +4,30 @@ import { getStringCSV } from "#/tests/utils/data";
 
 import { parseString } from "./parseString.js";
 
-describe.each([50, 100, 500, 1_000])("%d rows", (rows) => {
-  const string = getStringCSV(rows);
+describe.each([50, 100, 500, 1_000])("%d row", (row) => {
+  const string = getStringCSV(row);
 
-  bench("parse a string CSV to an AsyncIterableIterator", async () => {
+  bench(`parse a string ${row} CSV to an AsyncIterableIterator`, async () => {
     for await (const _ of parseString(string)) {
       // noop
     }
   });
 
-  bench("parse a string CSV to an Array", () => {
+  bench(`parse a string ${row} CSV to an Array`, () => {
     parseString.toArraySync(string);
   });
 
-  bench("parse a string CSV to a Promise<Array>", async () => {
+  bench(`parse a string ${row} CSV to a Promise<Array>`, async () => {
     await parseString.toArray(string);
   });
 
-  bench("parse a string CSV to a IterableIterator", () => {
+  bench(`parse a string ${row} CSV to a IterableIterator`, () => {
     for (const _ of parseString.toIterableIterator(string)) {
       // noop
     }
   });
 
-  bench("parse a string CSV to a ReadableStream", async () => {
+  bench(`parse a string ${row} CSV to a ReadableStream`, async () => {
     await parseString.toStream(string).pipeTo(
       new WritableStream({
         write(_) {
