69d6dab3f2d7770175bec0700b501e0f2a71dc34
kamiazya <yuki@kamiazya.tech>
2024-09-14 23:13:07 +0900

    Update GitHub workflows for wasm build artifacts

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         inlineDynamicImports: false,
-        preserveModules: true,
         preserveModulesRoot: "src",
         exports: "named",
       },
